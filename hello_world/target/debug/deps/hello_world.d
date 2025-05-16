60d25e7d95cdd60b0c4bd735b41059f3576d66f4
Yussufkadir <ysyurme1@stu.vistula.edu.pl>
2025-05-16 22:17:50 +0200

    Initial commit

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\yusuf\RustroverProjects\Rust-Learning-Path\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\yusuf\RustroverProjects\Rust-Learning-Path\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\yusuf\RustroverProjects\Rust-Learning-Path\hello_world\target\debug\hello_world.exe: C:\Users\yusuf\RustroverProjects\Rust-Learning-Path\hello_world\src\main.rs
